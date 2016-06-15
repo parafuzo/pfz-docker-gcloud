884bb7fecaf33ec62fac17a3cd5b98c57ffa457c
Elber Ribeiro <elber@parafuzo.com>
2016-06-15 17:57:53 -0300

    Update image script

--- image-update.sh
+++ image-update.sh
@@ -1,18 +1,24 @@
 #! /bin/bash
-echo "Git clone infra"
-git clone https://$GITHUB_TOKEN@github.com/parafuzo/infra.git
 
-echo "Moving to infra/$REPO_PATH"
-cd infra/$REPO_PATH
+endnow() {
+  echo "$1" && exit 1
+}
+
+echo "Git clone infra"
+git clone https://$GITHUB_TOKEN@github.com/parafuzo/infra.git || endnow "Git clone fail."
+
+DIR = "infra/$REPO_PATH"
+echo "Moving to $DIR"
+[ -d ${DIR}] && cd $DIR || endnow "\$DIR: $DIR does not exist."
 
 echo "Remote info"
 git remote -v
 
 echo "Replace image to $IMAGE_NAME"
-sed -i -e 's/image:.*/image: '$IMAGE_NAME'/g' deployment.yml
+sed -i 's/image:.*/image: '$IMAGE_NAME'/g' deployment.yml || endnow "Fail to replace image name."
 
 echo "Kube APPLY"
-kubectl apply -f deployment.yml
+kubectl apply -f deployment.yml || endnow "Fail to apply deployment new image name"
 
-git commit -am "New deployment image"
-git push
+git commit -am "New deployment image" || endnow "Fail to commit new image name"
+git push || endnow "Fil to push new image name to repo"
