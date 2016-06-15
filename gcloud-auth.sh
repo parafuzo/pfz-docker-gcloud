54f512ee8248a09d3ba844b38fb5d286ebf6dac8
Julio Turolla <julio.turolla@gmail.com>
2016-06-15 12:07:50 -0300

    Auth kubernetes on cluster

--- gcloud-auth.sh
+++ gcloud-auth.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 echo $GCLOUD_KEY | base64 -d > gcloud.p12
+
 gcloud auth activate-service-account $GCLOUD_EMAIL --key-file gcloud.p12
+gcloud config set compute/region $GCLOUD_REGION
+gcloud config set compute/zone $GCLOUD_ZONE
+gcloud container clusters get-credentials $GCLOUD_CLUSTER
+kubeclt get nodes
+
 ssh-keygen -f ~/.ssh/google_compute_engine -N ""
